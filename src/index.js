99f7ce97d8a996c5b8607c65057abe28092c9440
Matt Lynch <matthew_lynch92@hotmail.com>
2017-12-18 21:46:39 +0000

    fix path issues

--- src/index.js
+++ src/index.js
@@ -12,31 +12,31 @@ const defaults = {
 
 function preactCliCriticalCssPlugin(config, env, options) {
 	if (!config) {
-		throw new Error('You must pass a webpack config to preactCliCriticalCssPlugin');
+		throw new Error("You must pass a webpack config to preactCliCriticalCssPlugin");
 	}
 
 	if (!env) {
-		throw new Error('You must pass the env to preactCliCriticalCssPlugin');
+		throw new Error("You must pass the env to preactCliCriticalCssPlugin");
 	}
 
 	const opts = options || {};
 
 	if (!env.ssr && env.production && env.prerender && env.prerenderUrls) {
-		const routes = require(env.prerenderUrls);
+		const routes = require(path.resolve(env.cwd, env.prerenderUrls));
 		const fileName = "index.html";
 
 		routes
 			.map(route => {
 				if (route.url === "/") {
-					return `.${route.url}${fileName}`;
+					return path.resolve(env.dest, `.${route.url}${fileName}`);
 				}
 
-				return `.${route.url}/${fileName}`;
+				return path.resolve(env.dest, `.${route.url}/${fileName}`);
 			})
 			.forEach(filePath => {
 				config.plugins.push(
 					new HtmlCriticalPlugin(
-						merge([
+						merge.all([
 							defaults,
 							opts,
 							{
