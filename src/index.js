4e5d699371494bf329ad2712f56b5fd7760a11d9
Matt Lynch <matthew_lynch92@hotmail.com>
2018-02-13 22:20:43 +0000

    format code + minor tweaks to readme

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const path = require("path");
-const HtmlCriticalPlugin = require("html-critical-webpack-plugin");
-const merge = require("deepmerge");
+const path = require('path');
+const HtmlCriticalPlugin = require('html-critical-webpack-plugin');
+const merge = require('deepmerge');
 
 const defaults = {
 	inline: true,
@@ -12,22 +12,24 @@ const defaults = {
 
 function preactCliCriticalCssPlugin(config, env, options) {
 	if (!config) {
-		throw new Error("You must pass a webpack config to preactCliCriticalCssPlugin");
+		throw new Error(
+			'You must pass a webpack config to preactCliCriticalCssPlugin'
+		);
 	}
 
 	if (!env) {
-		throw new Error("You must pass the env to preactCliCriticalCssPlugin");
+		throw new Error('You must pass the env to preactCliCriticalCssPlugin');
 	}
 
 	const opts = options || {};
 
 	if (!env.ssr && env.production && env.prerender && env.prerenderUrls) {
 		const routes = require(path.resolve(env.cwd, env.prerenderUrls));
-		const fileName = "index.html";
+		const fileName = 'index.html';
 
 		routes
 			.map(route => {
-				if (route.url === "/") {
+				if (route.url === '/') {
 					return path.resolve(env.dest, `.${route.url}${fileName}`);
 				}
 
@@ -43,7 +45,7 @@ function preactCliCriticalCssPlugin(config, env, options) {
 								src: filePath,
 								dest: filePath
 							},
-							opts,
+							opts
 						])
 					)
 				);
